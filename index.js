347316592c5198db2a4ce7cb68df7aad8bed544d
Naman Dwivedi <namandwivedi14@gmail.com>
2022-06-26 10:05:37 -0400

    add links

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ import { ethers } from "https://cdnjs.cloudflare.com/ajax/libs/ethers/5.6.9/ethe
 const LIVEPEER_API_KEY = "96723baa-ee6f-4c6b-869b-0a110f8e27a6"
 
 const urlParams = new URLSearchParams(window.location.search);
-const username = urlParams.get('u');
+let username = urlParams.get('u');
+
+if (username == '') {
+  username = 'naman'
+}
 
 console.log('user: ' + username)
 
